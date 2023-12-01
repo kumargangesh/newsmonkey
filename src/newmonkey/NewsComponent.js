07e5926b3ed41f87c69aa9138c3ecd408db294c0
KumarGangesh123 <kumargangesh090@gmail.com>
2023-12-01 13:59:08 +0530

    API updation

--- src/newmonkey/NewsComponent.js
+++ src/newmonkey/NewsComponent.js
@@ -4,7 +4,7 @@ import "./Style.css";
 class NewsComponent extends Component {
   render() {
 
-    let {title, movieYear, imageURL} = this.props; // destructuring in JavaScript
+    let {title, description, imageURL} = this.props; // destructuring in JavaScript
 
     return (
       <div className="container">
@@ -12,7 +12,7 @@ class NewsComponent extends Component {
           <img src={imageURL} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
-            <p className="card-text">{movieYear}...</p>
+            <p className="card-text">{description}...</p>
             {/* <a href={newsURL} target="_blank" className="btn btn-primary">Read More</a> */}
           </div>
         </div>
